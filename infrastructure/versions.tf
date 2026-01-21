69b42e27ad07521188536290cd95f8a3ceef9230
akshaya.p <akshayaparamatma@gmail.com>
2026-01-22 00:56:44 +0530

    Added S3 to store terraform state values

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  # Remote backend - stores state in S3
+  backend "s3" {
+    bucket = "cloudops-inventory-tfstate-304450758375"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
